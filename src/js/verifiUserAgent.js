ca792a3b4faf149b14ed299fa907c3b5f102852b
Braian Palacios <braianpalacios699@gmail.com>
2025-01-13 00:01:56 -0300

    Se elimino archivo innecesario

--- src/js/verifiUserAgent.js
+++
@@ -1,6 +0,0 @@
-const isMobileOrTablet = () => {
-    const userAgent = navigator.userAgent.toLowerCase();
-    return /iphone|ipod|ipad|android|blackberry|mobile|tablet/.test(userAgent);
-};
-
-export default isMobileOrTablet;
